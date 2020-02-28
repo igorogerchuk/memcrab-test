c2b4e74a7667c79b3bb781332b2fd542cd832c9f
Igor <igor.ogerchuk@gmail.com>
2020-02-28 17:16:49 +0200

    build folder added

---
+++ build/precache-manifest.031506e1d959930dc059ef999b133abc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a66f34953c1cb0e78da2d8dcd753de1",
+    "url": "/memcrab-test/index.html"
+  },
+  {
+    "revision": "43212b633a63b8a198b8",
+    "url": "/memcrab-test/static/css/main.0b8517a3.chunk.css"
+  },
+  {
+    "revision": "c2ac071eef4981cca285",
+    "url": "/memcrab-test/static/js/2.0e494977.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/memcrab-test/static/js/2.0e494977.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43212b633a63b8a198b8",
+    "url": "/memcrab-test/static/js/main.511ed430.chunk.js"
+  },
+  {
+    "revision": "7b6cb94a2f89a3631690",
+    "url": "/memcrab-test/static/js/runtime-main.026b6c67.js"
+  }
+]);
