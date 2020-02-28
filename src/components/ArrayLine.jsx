bc425eb4b4cb90962e7bf031a24b40a2c2a449c2
Igor <igor.ogerchuk@gmail.com>
2020-02-28 13:35:49 +0200

    working version2

--- src/components/ArrayLine.jsx
+++ src/components/ArrayLine.jsx
@@ -6,66 +6,61 @@ import * as selectors from "../redux/selectors";
 
 const TdSum = styled.td`
   background-color: cadetblue;
-`;
-
-const Td = styled.td`
   cursor: pointer;
 `;
 
-//background: ${props => props.background};
+const Td = styled.td`
+  background: ${props => props.background};
+  cursor: pointer;
+`;
 
 class ArrayLine extends Component {
-  // state = { hover: false, sum: 0 };
+  state = { hover: false, sum: 0 };
 
   increaseHandler = e => {
-    // const { id } = e.target;
-    // if (id.slice(0, 6) !== "number") {
-    //   return;
-    // }
     const { onIncrease } = this.props;
     onIncrease(e.target.id);
   };
 
-  // hoverHandler = e => {
-  // const value = Number(e.target.innerText);
-  // this.setState(state => {
-  //   return {
-  //     hover: !state.hover,
-  //     sum: value
-  //   };
-  // });
-  // };
+  hoverHandler = e => {
+    const value = Number(e.target.innerText);
+    this.setState(state => {
+      return {
+        hover: !state.hover,
+        sum: value
+      };
+    });
+  };
 
   render() {
     const { line, onRemove } = this.props; // closestNumbers,
-    // const { hover, sum } = this.state;
+    const { hover, sum } = this.state;
 
     return (
       <tr>
         {line.map(element => {
-          // let bg = "white";
-          // const percents = (element.amount / sum) * 100;
+          let bg = "white";
+          const percents = (element.amount / sum) * 100;
           // if (closestNumbers.includes(element)) {
           //   bg = "green";
           // }
-          // if (hover) {
-          //   bg = `linear-gradient(to top, #fff200, #1e9600 ${percents}%, transparent ${percents}%)`;
-          // }
+          if (hover) {
+            bg = `linear-gradient(to top, #fff200, #1e9600 ${percents}%, transparent ${percents}%)`;
+          }
           return (
             <Td
               key={element.id}
               id={element.id}
               onClick={this.increaseHandler}
-              // background={bg}
+              background={bg}
             >
-              {/* {hover ? percents.toFixed(1) + "%" : element.amount} */}
-              {element.amount}
+              {hover ? percents.toFixed(1) + "%" : element.amount}
             </Td>
           );
         })}
         <TdSum
-        // onMouseEnter={this.hoverHandler}
-        // onMouseLeave={this.hoverHandler}
+          onMouseEnter={this.hoverHandler}
+          onMouseLeave={this.hoverHandler}
         >
           {line.reduce((sum, element) => (sum += element.amount), 0)}
         </TdSum>
@@ -76,12 +71,6 @@ class ArrayLine extends Component {
     );
   }
 }
-
-// const mapStateToProps = state => {
-//   return {
-//     array: selectors.getArray(state)
-//   };
-// };
 
 const mapDispatchToProps = (dispatch, ownProps) => {
   return {
