bc2fbd49746496f6e27ea268dc908c1179eb0d22
Igor <igor.ogerchuk@gmail.com>
2020-03-02 01:16:39 +0200

    Updates

---
+++ precache-manifest.adec390a177c1e2d2ea87ecc87e8dbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb2803af35c9dfca9720ce4ecdefe577",
+    "url": "/memcrab-test/index.html"
+  },
+  {
+    "revision": "355ba19f7505b88813d3",
+    "url": "/memcrab-test/static/css/main.27849797.chunk.css"
+  },
+  {
+    "revision": "2a7e175cd8b34956516f",
+    "url": "/memcrab-test/static/js/2.e5000b6a.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/memcrab-test/static/js/2.e5000b6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "355ba19f7505b88813d3",
+    "url": "/memcrab-test/static/js/main.bd844722.chunk.js"
+  },
+  {
+    "revision": "7b6cb94a2f89a3631690",
+    "url": "/memcrab-test/static/js/runtime-main.026b6c67.js"
+  }
+]);
