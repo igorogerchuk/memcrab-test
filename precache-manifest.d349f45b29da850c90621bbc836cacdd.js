a3940a6a20fa10c457f6af999d49bdee76f9c691
Igor <igor.ogerchuk@gmail.com>
2020-03-03 12:48:06 +0200

    Updates

--- precache-manifest.d349f45b29da850c90621bbc836cacdd.js
+++ precache-manifest.d349f45b29da850c90621bbc836cacdd.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "83b3500b7f43b53e06b430a0b6d1dab8",
+    "revision": "61075eacc2b1611a48e11d17032fba35",
     "url": "/memcrab-test/index.html"
   },
   {
-    "revision": "34f9a19590ced9e45caf",
-    "url": "/memcrab-test/static/css/main.fd7eec98.chunk.css"
+    "revision": "5fbae55b7b939e70dcc6",
+    "url": "/memcrab-test/static/css/main.afc6a8b8.chunk.css"
   },
   {
     "revision": "325d6c36f5056bad9bba",
@@ -16,7 +16,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/memcrab-test/static/js/2.a5313059.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "34f9a19590ced9e45caf",
+    "revision": "5fbae55b7b939e70dcc6",
     "url": "/memcrab-test/static/js/main.1c93acf7.chunk.js"
   },
   {
